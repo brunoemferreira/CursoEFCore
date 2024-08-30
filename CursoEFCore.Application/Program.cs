d7d84b1bc73e8edf4d00d4845210a4e4c54c19f2
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-08-30 14:16:40 -0300

    feat: Insert Data on Database

---
+++ CursoEFCore.Application/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using CursoEFCore.Model.Models;
+using CursoEFCore.Model.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoEFCore.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Faz a Migration
+            // StartMigration();
+
+            // Insere o Registro no banco de Dados 
+            InserirDados();
+        }
+
+        private static void StartMigration()
+        {
+            using var db = new Data.Context.ApplicationContext();
+
+            var existsPendingMigrations = db.Database.GetPendingMigrations().Any();
+            if (existsPendingMigrations)
+            {
+                Console.WriteLine("Existe Migration Pendente, Não foi possível Aplicar as Novas Migrations");
+            }
+            else
+            {
+                db.Database.Migrate();
+            }
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "12345612345651",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new Data.Context.ApplicationContext();
+
+            /*  Formas de rastrear para Inserir o Produto no Banco de Dados 
+             *  As 1ª e 2ª Formas São as mais indicadas 
+             */
+            // 1ª Forma
+            db.Produtos.Add(produto);
+            // 2ª Forma 
+            //db.Set<Produto>().Add(produto);
+            // 3ª Forma 
+            //db.Entry(produto).State = EntityState.Added;
+            // 4ª Forma 
+            //db.Add(produto);
+
+            // Pega tudo que esta sendo rastreado pelo Entity framework para enviar para o Banco de Dados
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total de registro(s): {registros}");
+        }
+    }
+}
--- CursoEFCore.Data/Context/ApplicationContext.cs
+++ CursoEFCore.Data/Context/ApplicationContext.cs
@@ -6,9 +6,12 @@ namespace CursoEFCore.Data.Context
     public class ApplicationContext : DbContext
     {
         public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoItem> PedidoItens { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-     => optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=cursoEFCore;User Id=root;Password=;");
+     => optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=cursoEFCore;User Id=postgres;Password=123456;");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
